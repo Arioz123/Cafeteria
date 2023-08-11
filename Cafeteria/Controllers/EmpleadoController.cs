26ca0512e887a8260aa6be01ab545623e3b9f711
ferdhy <ferdhy.213@gmail.com>
2023-08-11 13:21:45 -0700

    Conexion

---
+++ Cafeteria/Controllers/EmpleadoController.cs
@@ -0,0 +1,20 @@
+﻿using Cafeteria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafeteria.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        EmpleadoModel _empleadoModel= new EmpleadoModel();
+       
+        public IActionResult Index()
+        {
+            var Consulta_trabajador = _empleadoModel.Puesto(), _empleadoModel.Salario();
+            //Devolvera el puesto y el salario del empleado
+            return View(Consulta_trabajador);
+
+            
+        }
+
+    }
+}
---
+++ Cafeteria/Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Cafeteria.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSql = String.Empty;
+        
+            public Conexion()
+            {
+                var Builder = new ConfigurationBuilder().SetBasePath
+                    (Directory.GetCurrentDirectory()).AddJsonFile
+                    ("appsettings.json").Build();
+                cadenaSql = Builder.GetSection("ConnectionStrings:cadenaSql").Value;
+            }
+
+            public string getCadenaSql()
+            {
+                return cadenaSql;
+            }
+    }
+        
+}
+
