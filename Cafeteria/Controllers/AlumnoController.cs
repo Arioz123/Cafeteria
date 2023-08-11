db5094ab4e2d9d0e2edb5ab7d87ece8a28212cbd
Tncsito <eduarosantoyo@gmail.com>
2023-08-11 13:06:35 -0700

    .

---
+++ Cafeteria/Controllers/AlumnoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cafeteria.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Index() //Esto es para el inicio de sesión (Esta obviamente es la pagina principal)
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarAlumno() //Esta es la pagina para registrar al alumno (No hay para empleado, se sube directamente desde la base de datos)
+        {
+            return View();
+        }
+    }
+}
