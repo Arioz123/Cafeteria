b480dd896ee8dca19f9f4e6a2b7c3ef618fb8c10
Arleth <arletvalle78@gmail.com>
2023-08-11 13:26:40 -0700

    Cree el controlador

---
+++ Cafeteria/Controllers/ProductosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cafeteria.Controllers
+{
+    public class ProductosController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
