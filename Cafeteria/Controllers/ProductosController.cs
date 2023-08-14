ae020e599e58bcd2b6f069769318a0d5be1f88f0
cisco01 <cisco01@CISCO1>
2023-08-14 12:44:59 -0700

    .

---
+++ Cafeteria/Controllers/ProductosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductoDatos
+
+namespace Cafeteria.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
